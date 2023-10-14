7acde08eb7c08818cca0828ab735fd87fd783f65
Patrik Holba <Paveegamedev@gmail.com>
2023-10-14 21:23:34 +0200

    Change line WIP - debug

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import movement
 from pybricks.ev3devices import Motor, ColorSensor
 from pybricks.parameters import Port
 from pybricks.tools import wait
-from pybricks.robotics import DriveBase
+from pybricks.hubs import EV3Brick
 
 # Initialize the motors and sensors.
 leftMotor = Motor(Port.B)
 rightMotor = Motor(Port.C)
 lineSensor = ColorSensor(Port.S3)
 
+ev3 = EV3Brick()
+
 while True:
     movement.moveByLine(leftMotor, rightMotor, lineSensor)
+    if ev3.buttons.pressed().length > 0:
+        movement.changeLine()
     wait(10)
--- movement.py
+++ movement.py
@@ -1,3 +1,5 @@
+from pybricks.robotics import DriveBase
+
 BLACK = 9
 WHITE = 85
 threshold = (BLACK + WHITE) / 2
@@ -5,19 +7,22 @@ DRIVE_SPEED = 100
 PROPORTIONAL_GAIN = 1.2
 isLeft = True
 
+
 def drive(robot, speed, turnRate):
     robot.drive(speed, turnRate)
 
 
 def moveByLine(leftMotor, rightMotor, lineSensor):
-
+    multiplier = 1
+    if isLeft:
+        multiplier = -1
     deviation = lineSensor.reflection() - threshold
     turn_rate = PROPORTIONAL_GAIN * deviation
     robot = DriveBase(leftMotor, rightMotor, wheelDiameter=55.5, axleTrack=104)
-    drive(robot, DRIVE_SPEED, turn_rate)
+    drive(robot, DRIVE_SPEED, turn_rate * multiplier)
 
 
 def changeLine():
     global isLeft
     isLeft = not isLeft
-    print (isLeft)
+    print("Changing lines. Am I going left? :" + isLeft)
