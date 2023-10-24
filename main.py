2d875eb14a2f99aa602202fdbb8fd1170a2d728f
Patrik Holba <Paveegamedev@gmail.com>
2023-10-24 21:36:24 +0200

    dropOff prep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.ev3devices import Motor, ColorSensor, GyroSensor
 from pybricks.parameters import Port
-from pybricks.tools import wait
 from pybricks.robotics import DriveBase
 from pybricks.hubs import EV3Brick
 import time
@@ -16,7 +15,7 @@ ev3 = EV3Brick()
 
 
 # constants
-DEG_TO_RAD = math.pi/180
+DEG_TO_RAD = math.pi / 180
 BLACK = 5
 WHITE = 30
 threshold = (BLACK + WHITE) / 2
@@ -37,9 +36,9 @@ current_time_from_start = 0
 last_gyro_check_time = current_time
 gyro_angle = 0
 turn_list = []
-can_go_middle = 0
-turn_rate_multyplier = 1
-going_middle = 0
+can_go_middle = False
+turn_rate_multiplier = 1
+isInMiddle = False
 
 
 def writeTurn(isLeft):
@@ -50,103 +49,103 @@ def writeTurn(isLeft):
 def checkGyroMovements():
     if gyro_sensor.angle() > 85:
         writeTurn(0)
-        gyro_sensor.reset_angle(0)
+        gyro_sensor.reset_angle(2)
     elif gyro_sensor.angle() < -85:
         writeTurn(1)
-        gyro_sensor.reset_angle(0)
+        gyro_sensor.reset_angle(-2)
 
 
 def goMiddle():
     global can_go_middle
     global robot
-    global turn_rate_multyplier
-    global going_middle
+    global turn_rate_multiplier
+    global isInMiddle
     print("going middle")
     robot.stop()
-    robot.straight(100)
     robot.turn(50)
-    turn_rate_multyplier = -1
-    can_go_middle = 0
-    going_middle = 0
+    turn_rate_multiplier = -1
+    can_go_middle = False
+    isInMiddle = True
 
-
-def tryGoMiddle():
-    global going_middle
-    last_three_items = turn_list[-3:]
-    if last_three_items == [0, 0, 1]:
-        going_middle = 1
-
+def dropOff():
+    print("Dropping the cubes rn")
 
 def calculateGoMiddle():
     global can_go_middle
-    last_three_items = turn_list[-3:]
+    global isInMiddle
 
-    if last_three_items == [1, 0, 1]:
-        can_go_middle = 1
+    if isInMiddle: return
+    last_six_items = turn_list[-6:]
+
+    if last_six_items == [1, 0, 1, 0, 0, 1]:
+        can_go_middle = True
+    else:
+        can_go_middle = False
+
 
 ##ROBO DPS (- Davis Positioning System)
 class DPS_class:
-  def __init__(self, x, y):
-    self.time = time.time()
-    self.angle = 0
-    self.x = 0
-    self.y = 0
+    def __init__(self, x, y):
+        self.time = time.time()
+        self.angle = 0
+        self.x = 0
+        self.y = 0
 
-    def calc(self, speed, turning_rate):
-        deltaT = self.time - time.time()
-        if turning_rate != 0:
-            radius = (180 * speed)/(math.pi * turning_rate)
-            alpha = turning_rate * deltaT
+        def calc(self, speed, turning_rate):
+            deltaT = self.time - time.time()
+            if turning_rate != 0:
+                radius = (180 * speed) / (math.pi * turning_rate)
+                alpha = turning_rate * deltaT
 
-            X = math.sin(alpha * DEG_TO_RAD) * radius
-            if 90 < turning_rate * deltaT and turning_rate * deltaT < 270
-                Y = radius + math.cos(alpha * DEG_TO_RAD) * radius
-            else:
-                Y = radius - math.cos(alpha * DEG_TO_RAD) * radius
-            ##stred kruznice po ktere robot jede neni v bode [0,0] ale je o polomer ve smeru Y posunuty
+                X = math.sin(alpha * DEG_TO_RAD) * radius
+                if 90 < turning_rate * deltaT and turning_rate * deltaT < 270:
+                    Y = radius + math.cos(alpha * DEG_TO_RAD) * radius
+                else:
+                    Y = radius - math.cos(alpha * DEG_TO_RAD) * radius
+                ##stred kruznice po ktere robot jede neni v bode [0,0] ale je o polomer ve smeru Y posunuty
 
-            if self.angle != 0:
-                radius2 = math.sqrt(Y**2 + X**2)
-                self.angle += math.asin(Y/radius2)/DEG_TO_RAD
-                self.x += math.cos(self.angle * DEG_TO_RAD) * radius2
-                self.y += math.sin(self.angle * DEG_TO_RAD) * radius2
-            else:
-                self.angle += alpha
-                self.x += X
-                self.y += Y
-            self.time = time.time()
-        else:##robot jede po rovny primce, ktera je pod uhlem self.angle
-            self.x += math.cos(self.angle * DEG_TO_RAD) * (speed * deltaT)
-            self.y += math.sin(self.angle * DEG_TO_RAD) * (speed * deltaT)
-            self.time = time.time()
-
+                if self.angle != 0:
+                    radius2 = math.sqrt(Y**2 + X**2)
+                    self.angle += math.asin(Y / radius2) / DEG_TO_RAD
+                    self.x += math.cos(self.angle * DEG_TO_RAD) * radius2
+                    self.y += math.sin(self.angle * DEG_TO_RAD) * radius2
+                else:
+                    self.angle += alpha
+                    self.x += X
+                    self.y += Y
+                self.time = time.time()
+            else:  ##robot jede po rovny primce, ktera je pod uhlem self.angle
+                self.x += math.cos(self.angle * DEG_TO_RAD) * (speed * deltaT)
+                self.y += math.sin(self.angle * DEG_TO_RAD) * (speed * deltaT)
+                self.time = time.time()
 
 
 # Start following the line endlessly.
-DPS = DPS_class(0,0)
+DPS = DPS_class(0, 0)
 while True:
     current_time = time.time()
     current_time_from_start = current_time - START_TIME
     checkGyroMovements()
 
-    if current_time_from_start > 80:
+    if current_time_from_start > 82:
         ev3.speaker.beep()
         break
 
-    if current_time_from_start > 70 and going_middle == 1:
+    if current_time_from_start > 10 and can_go_middle:
         goMiddle()
 
+    if isInMiddle and turn_list[-2] == [0,0]:
+
+
     calculateGoMiddle()
-    if can_go_middle == 1:
-        tryGoMiddle()
 
     deviation = line_sensor.reflection() - threshold
 
     # Calculate the turn rate.
-    turn_rate = deviation * abs(deviation) / TURN_RATE_DIVIDER * turn_rate_multyplier
+    turn_rate = deviation * abs(deviation) / TURN_RATE_DIVIDER * turn_rate_multiplier
     # print("turn rate: " + str(turn_rate))
 
-    #updates robot positioning system !!!!.NEEDS to be JUST BEFORE .drive()!!!
-    DPS.calc(DRIVE_SPEED, turn_rate)
+    # updates robot positioning system !!!!.NEEDS to be JUST BEFORE .drive()!!!
+    # DPS.calc(DRIVE_SPEED, turn_rate)
     # Set the drive base speed and turn rate.
     robot.drive(DRIVE_SPEED, turn_rate)
