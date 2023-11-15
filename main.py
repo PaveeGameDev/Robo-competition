757f3522dea46c3eb51f60b7f36a51e80dd9f90f
Out Stone <dauxid@gmail.com>
2023-11-15 17:28:10 +0100

    uplne blba chyba byla fixla

--- main.py
+++ main.py
@@ -91,17 +91,19 @@ class DPS_class:
         self.y = y
         self.speed = 0
         self.turning = 0
+        self.gyro = 0
 
     def calc(self, speed, turning_rate):
         deltaT = time.time() - self.time
         self.time = time.time()
         robot.drive(speed, turning_rate)
-        print("time " + str(deltaT))
+        gyro1 = gyro_sensor.angle()
 
         beta = self.turning * deltaT
-        difference = gyro_sensor.angle() - (self.angle + beta)
+        difference = self.gyro - (self.angle + beta)
         if abs(difference) >= 2:
             print("fixing " + str(difference))
+            beta += difference
 
         if self.turning != 0:
             radius = (180 * self.speed) / (math.pi * self.turning)
@@ -115,6 +117,7 @@ class DPS_class:
             self.x += math.cos(self.angle * DEG_TO_RAD) * self.speed * deltaT
             self.y += math.sin(self.angle * DEG_TO_RAD) * self.speed * deltaT
 
+        self.gyro = gyro1
         self.speed = speed
         self.turning = turning_rate
 
@@ -155,22 +158,20 @@ class DPS_class:
         else:
             deltaAngle = alpha - mod(self.angle, 360)
             print("    turning")
-            print(alpha, deltaX, deltaY)
-            print(self.angle, self.x, self.y)
-            print(deltaAngle)
-            print(mod(self.angle, 360))
+            print(alpha)
+            print(self.angle)
             self.calc(
                 speed=0, turning_rate=deltaAngle * 2
             )  # ten nasobitel se bude menit - musi se najit nelepsi hodnota
 
-        if self.x + 5 > x and self.x - 5 < x and self.y + 5 > y and self.y - 5 < y:
+        if self.x + 10 > x and self.x - 10 < x and self.y + 10 > y and self.y - 10 < y:
             return True
         else:
             return False
 
 
 # Start following the line endlessly.
-DPS = DPS_class(1960, 1120)
+DPS = DPS_class(0, 0)
 check = False
 print(DPS.x, DPS.y, DPS.zelta, DPS.angle)
 destination = [points[ordered_points[0]][0], points[ordered_points[0]][1]]
@@ -189,21 +190,23 @@ while True:
     back = DPS.trajectory(destination[0], destination[1])
 
     if back:
-        i += 1
         if current_time_from_start > TIME_TO_MIDDLE and not wentToMiddle:
             needToGoMiddle()
+        i += 1
 
         if i < len(points):
-            destination = destination = [
+            print("#####--NEW DESTINATION--#####")
+            destination = [
                 points[ordered_points[i]][0],
                 points[ordered_points[i]][1],
             ]
+            
         else:
-            dropOff()
-            # robot.stop()
-            # ev3.speaker.beep()
-            # print(DPS.x, DPS.y, DPS.zelta, DPS.angle)
-            # break
+            #dropOff()
+            robot.stop()
+            ev3.speaker.beep()
+            print(DPS.x, DPS.y, DPS.zelta, DPS.angle)
+            break
 
     # grabber_motor.run(GRAB_SPEED)
 
